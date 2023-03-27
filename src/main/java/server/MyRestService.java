798e91977151531d11439a490db685c5d79ae5c4
Bernd Schatz <bernd.schatz@schaffa.org>
2023-03-27 20:40:34 +0200

    FIXED: removed needless annotation

--- src/main/java/server/MyRestService.java
+++ src/main/java/server/MyRestService.java
@@ -1,11 +1,9 @@
 package server;
 
-import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/rest")
 @Path("op")
 public class MyRestService extends Application
 {
